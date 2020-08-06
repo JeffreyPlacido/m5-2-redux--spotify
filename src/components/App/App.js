f9f1bfedbd41c36e81be7efd046208a265213fc6
Jeffrey <jeffreyplacido@hotmail.com>
2020-08-06 14:50:21 -0400

    E5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import GlobalStyles from "../GlobalStyles";
+
+import ArtistRoute from "../ArtistRoute";
+
+const DEFAULT_ARTIST_ID = "1LZEQNv7sE11VDY3SdxQeN";
 
 const App = () => {
   return (
     <Router>
+      <Switch>
+        <Route exact path="/artists/:id">
+          <ArtistRoute />
+        </Route>
+        <Redirect to={`/artists/${DEFAULT_ARTIST_ID}`} />
+      </Switch>
       <GlobalStyles />
-      trying to find styles
     </Router>
   );
 };
